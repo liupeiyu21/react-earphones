029decbd4b74e38de9efacf23b547fac6a6245e2
Liu Peiyu <24aw0142@jec.ac.jp>
2024-12-03 17:23:06 +0900

    ファスビューの基本を終わりました。

---
+++ src/js/top/fasview.jsx
@@ -0,0 +1,175 @@
+import { Swiper, SwiperSlide } from 'swiper/react';
+import { Navigation, Pagination, Scrollbar, A11y } from 'swiper/modules';
+
+import 'swiper/swiper-bundle.css'
+import '../../css/fasview.css'
+
+import Fasview from '../../assets/fasview-banner.png'
+import Fasviewpre from '../../assets/fasview-pre.png'
+import Fasviwenext from '../../assets/fasview-next.png'
+
+
+export const Slider = () => {
+
+    return (
+        <>
+        <Swiper
+          modules={[Navigation, Pagination, Scrollbar, A11y]}
+        //   spaceBetween={20}
+          slidesPerView={5}
+          navigation = {{
+            nextEl: '.pre',
+            prevEl: '.next'
+          }}
+
+          //
+          freeMode={{enabled:true}
+
+          }
+          pagination={{ clickable: true }}
+          scrollbar={{ draggable: true }}
+          loop = {{loop: true}}
+          onSlideChange={() => console.log('slide change')}
+          onSwiper={(swiper) => console.log(swiper)}
+        >
+          <SwiperSlide>
+            <div className='swiper-slide-content'>
+                 <img src={Fasview} alt="バナーです" />
+            </div>
+          </SwiperSlide>
+          <SwiperSlide>
+            <div className='swiper-slide-content'>
+                 <img src={Fasview} alt="バナーです" />
+            </div>
+          </SwiperSlide>
+          <SwiperSlide>
+            <div className='swiper-slide-content'>
+                 <img src={Fasview} alt="バナーです" />
+            </div>
+          </SwiperSlide>
+          <SwiperSlide>
+            <div className='swiper-slide-content'>
+                 <img src={Fasview} alt="バナーです" />
+            </div>
+          </SwiperSlide>
+          <SwiperSlide>
+            <div className='swiper-slide-content'>
+                 <img src={Fasview} alt="バナーです" />
+            </div>
+          </SwiperSlide>
+          <SwiperSlide>
+            <div className='swiper-slide-content'>
+                 <img src={Fasview} alt="バナーです" />
+            </div>
+          </SwiperSlide>
+          <SwiperSlide>
+            <div className='swiper-slide-content'>
+                 <img src={Fasview} alt="バナーです" />
+            </div>
+          </SwiperSlide>
+          <SwiperSlide>
+            <div className='swiper-slide-content'>
+                 <img src={Fasview} alt="バナーです" />
+            </div>
+          </SwiperSlide>
+          <SwiperSlide>
+            <div className='swiper-slide-content'>
+                 <img src={Fasview} alt="バナーです" />
+            </div>
+          </SwiperSlide>
+          <SwiperSlide>
+            <div className='swiper-slide-content'>
+                 <img src={Fasview} alt="バナーです" />
+            </div>
+          </SwiperSlide>
+        <div className='swiper-movebtn'>
+            <div className='pre'><img src={Fasviewpre} alt="" /></div>
+            <div className='next'><img src={Fasviwenext} alt="" /></div>
+        </div>
+        
+        </Swiper>
+
+        </>
+      );
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// // // Import Swiper React components
+// // import { Swiper, SwiperSlide } from 'swiper/react';
+// // import Fasview from '../../assets/fasview-banner.png'
+// // import { Navigation, Pagination, Scrollbar } from "swiper/modules";
+
+// // // Import Swiper styles
+// // import 'swiper/css';
+
+// //  function FasSwiper(){
+// //   return (
+// //     <>
+// //     <Swiper
+// //       spaceBetween={0}
+// //       slidesPerView={3}
+// //       onSlideChange={() => console.log('slide change')}
+// //       onSwiper={(swiper) => console.log(swiper)}
+// //       direction="vertical"
+// //       loop={true}
+// //       modules={[Navigation, Pagination, Scrollbar]}
+// //       pagination={{ clickable: true }}
+// //       navigation={{ nextEl: ".swiper-button-next", prevEl: ".swiper-button-prev" }}
+// //       scrollbar={{ draggable: true }}
+      
+// //     >
+// //       <SwiperSlide><img src={Fasview} alt="バナーです" /></SwiperSlide>
+// //       <SwiperSlide><img src={Fasview} alt="バナーです" /></SwiperSlide>
+// //       <SwiperSlide><img src={Fasview} alt="バナーです" /></SwiperSlide>
+// //       <SwiperSlide><img src={Fasview} alt="バナーです" /></SwiperSlide>
+// //       <SwiperSlide><img src={Fasview} alt="バナーです" /></SwiperSlide>
+// //       <SwiperSlide><img src={Fasview} alt="バナーです" /></SwiperSlide>
+// //       <SwiperSlide><img src={Fasview} alt="バナーです" /></SwiperSlide>
+// //       <SwiperSlide><img src={Fasview} alt="バナーです" /></SwiperSlide>
+// //       <SwiperSlide><img src={Fasview} alt="バナーです" /></SwiperSlide>
+// //       <SwiperSlide><img src={Fasview} alt="バナーです" /></SwiperSlide>
+// //     </Swiper>
+// //     <div class="swiper-pagination"></div>
+// //     <div class="swiper-button-prev"></div>
+// //     <div class="swiper-button-next"></div>
+// //     </>
+// //   );
+// // };
+
+// export default FasSwiper
